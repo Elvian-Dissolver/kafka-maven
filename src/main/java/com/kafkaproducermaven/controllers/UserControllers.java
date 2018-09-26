13c6925b8c4d0fe0ee6636e0862cffb4b0464287
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:41:31 +0700

    Add RestController dependency on Usercontroller class

--- src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
+++ src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
@@ -1,4 +1,9 @@
 package com.kafkaproducermaven.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
 public class UserControllers {
 }
