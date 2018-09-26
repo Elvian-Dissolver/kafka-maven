c1039703a88ccd41ffc68b231455a9471c8ca55a
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:58:13 +0700

    Add kafkaTemplate method

--- src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
+++ src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -30,5 +31,10 @@ public class KafkaConfiguration {
     public ProducerFactory<String, User> producerFactory() {
 
         return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, User> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
