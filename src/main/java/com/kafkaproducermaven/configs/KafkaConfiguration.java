d85fd4ed2d7ed8cce5b413962c512fb8c0637f28
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:57:25 +0700

    Add producerFactory method

--- src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
+++ src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
@@ -1,12 +1,14 @@
 package com.kafkaproducermaven.configs;
 
 
+import com.kafkaproducermaven.models.User;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,5 +24,11 @@ public class KafkaConfiguration {
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return config;
+    }
+
+    @Bean
+    public ProducerFactory<String, User> producerFactory() {
+
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 }
