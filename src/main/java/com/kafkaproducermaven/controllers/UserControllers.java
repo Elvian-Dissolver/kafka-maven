2da7e069180307b08e190b2cdfcd13ff0556fc5e
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:44:18 +0700

    Add method for Usercontroller class

--- src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
+++ src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
@@ -2,14 +2,26 @@ package com.kafkaproducermaven.controllers;
 
 import com.kafkaproducermaven.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("kafka")
 public class UserControllers {
-    @Autowired
+    //@Autowired
     //private KafkaTemplate<String, User> kafkaTemplate;
 
     private static final String TOPIC = "Kafka_Demo";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name){
+
+        //kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
+
+        return "Published successfully";
+
+    }
+
 }
