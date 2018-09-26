b3501f2dbeadd2c254ed84460d21fcc47a278816
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:54:39 +0700

    Add producerConfigs method and dependency

--- src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
+++ src/main/java/com/kafkaproducermaven/configs/KafkaConfiguration.java
@@ -1,4 +1,26 @@
 package com.kafkaproducermaven.configs;
 
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConfiguration {
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return config;
+    }
 }
