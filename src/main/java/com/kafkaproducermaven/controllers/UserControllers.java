238566dad36d84370715bebce385ca424ce2a20e
DissoLver <indra.elvian@gmail.com>
2018-09-27 03:42:46 +0700

    Add attribute for Usercontroller class

--- src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
+++ src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
@@ -1,9 +1,15 @@
 package com.kafkaproducermaven.controllers;
 
+import com.kafkaproducermaven.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("kafka")
 public class UserControllers {
+    @Autowired
+    //private KafkaTemplate<String, User> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Demo";
 }
