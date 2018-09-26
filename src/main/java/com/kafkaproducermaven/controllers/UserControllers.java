9bdc33529426abac0df61577ed1ba3399c73b241
DissoLver <indra.elvian@gmail.com>
2018-09-27 04:01:13 +0700

    change code rev

--- src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
+++ src/main/java/com/kafkaproducermaven/controllers/UserControllers.java
@@ -2,6 +2,7 @@ package com.kafkaproducermaven.controllers;
 
 import com.kafkaproducermaven.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("kafka")
 public class UserControllers {
-    //@Autowired
-    //private KafkaTemplate<String, User> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
 
     private static final String TOPIC = "Kafka_Demo";
 
     @GetMapping("/publish/{name}")
     public String post(@PathVariable("name") final String name){
 
-        //kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
+        kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
 
         return "Published successfully";
 
