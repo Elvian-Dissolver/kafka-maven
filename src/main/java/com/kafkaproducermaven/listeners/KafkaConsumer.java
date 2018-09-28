b236d4445861757ecaca230f1937e22ead8af1b0
DissoLver <indra.elvian@gmail.com>
2018-09-28 07:03:53 +0700

    Add consumeJson method in kafkaconsumer class

--- src/main/java/com/kafkaproducermaven/listeners/KafkaConsumer.java
+++ src/main/java/com/kafkaproducermaven/listeners/KafkaConsumer.java
@@ -1,4 +1,14 @@
 package com.kafkaproducermaven.listeners;
 
+import com.kafkaproducermaven.models.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaConsumer {
+    @KafkaListener(topics = "Kafka_Demo", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
 }
