fe84a7f672f85860f6e4695a1de066df08a52c6e
DissoLver <indra.elvian@gmail.com>
2018-09-30 21:05:55 +0700

    Add PostMapping method

--- src/main/java/com/kafkaproducermaven/producers/KafkaProducer.java
+++ src/main/java/com/kafkaproducermaven/producers/KafkaProducer.java
@@ -4,10 +4,7 @@ import com.kafkaproducermaven.models.User;
 import com.kafkaproducermaven.models.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("kafka")
@@ -32,5 +29,13 @@ public class KafkaProducer {
         return "Published successfully";
 
     }
+
+    @PostMapping("/insert")
+    public String insert(@RequestBody User user){
+        this.userRepository.insert(user);
+        kafkaTemplate.send(TOPIC, user);
+        return "Post successfully";
+    }
+
 
 }
